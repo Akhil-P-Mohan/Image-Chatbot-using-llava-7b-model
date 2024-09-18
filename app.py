e2f238627c857210ca3d73e8c7efbfbeb47961b1
Akhil-P-Mohan <akhilpmohan17@gmail.com>
2024-09-18 16:24:10 +0530

    new

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#app.py
+# app.py
 
 from flask import Flask, request, render_template, send_file, jsonify
 from PIL import Image
@@ -13,32 +13,48 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-# Define a route for handling image uploads and questions
+# Define a route for handling questions
 @app.route("/ask", methods=["POST"])
 def ask():
     try:
-        image_file = request.files["image"]
-        question = request.form["question"]
-
-        # Save the image to a temporary file
-        filename = secure_filename(image_file.filename)
-        image_path = os.path.join("static", "images", filename)
-        image_file.save(image_path)
-
-        # Use the ollama.chat function to generate a response
-        res = ollama.chat(
-            model="llava:7b",
-            messages=[
-                {
+        
+        # Check if an image is provided
+        if "image" in request.files:
+            image_file = request.files["image"]
+            filename = secure_filename(image_file.filename)
+            if filename:  # Check if filename is not empty
+                image_path = os.path.join("static", "images", filename)
+                image_file.save(image_path)
+                question = ""
+                message = {
                     'role': 'user',
                     'content': question,
                     'images': [image_path]
                 }
-            ]
+            else:
+                question = request.form["question"]
+                message = {
+                    'role': 'user',
+                    'content': question
+                }
+        else:
+            question = request.form["question"]
+            message = {
+                'role': 'user',
+                'content': question
+            }
+
+        
+
+        # Use the ollama.chat function to generate a response
+        res = ollama.chat(
+            model="llava:7b",
+            messages=[message]
+            
         )
 
         # Return the response as a JSON object
-        return jsonify({'response': res['message']['content']})
+        return jsonify(res['message']['content'])
     except Exception as e:
         return jsonify({'error': str(e)})
 
